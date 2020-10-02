6a9645f84a523867bbf8ca1803107a6baab28634
uvsq21802951 <vincent.xavier@ens.uvsq.fr>
2020-10-02 09:24:02 +0200

    Modif classe Fraction

--- SimpleFraction/src/pack/Main.java
+++ SimpleFraction/src/pack/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Fraction fraction = new Fraction(2,5);
-		fraction.afficher();
+		System.out.println(fraction.toString());
 	}
 	
 }
